0fd4734a387109b0460db0490177d8547c40b376
Anthony Churpita <32397472+churpita@users.noreply.github.com>
2023-08-01 20:34:46 -0400

    Add player tables

--- server/sql/0001-tables.sql
+++ server/sql/0001-tables.sql
@@ -2,20 +2,31 @@ CREATE TABLE tournament (
     tournament_key          int             NOT NULL AUTO_INCREMENT,
     name                    varchar(128)    NOT NULL,
     description             varchar(1024)   NULL,
+
     PRIMARY KEY (tournament_key)
 );
 
 CREATE TABLE match (
     match_key               int             NOT NULL AUTO_INCREMENT,
     winner_team_key         int             NULL,
+    
     PRIMARY KEY (match_key)
 );
 
 CREATE TABLE team (
     team_key                int             NOT NULL AUTO_INCREMENT,
     name                    varchar(64)     NOT NULL,
+    
     PRIMARY KEY (team_key)
 );
+
+CREATE TABLE player (
+    player_key              int             NOT NULL AUTO_INCREMENT,
+    username                varchar(128)    NOT NULL,
+    discord_name            varchar(32)     NULL,
+    
+    PRIMARY KEY (player_key)
+)
 
 -- Foreign key tables
 
@@ -27,7 +38,7 @@ CREATE TABLE tournament_match (
     previous_match_key      int             NULL,
     round                   int             NULL,
     seed                    int             NULL,
-
+    
     PRIMARY KEY (tournament_match_key),
     FOREIGN KEY (tournament_key) REFERENCES tournament(tournament_key),
     FOREIGN KEY (match_key) REFERENCES match(match_key),
@@ -35,11 +46,24 @@ CREATE TABLE tournament_match (
 );
 
 -- Consider this table the participants of any given match
-CREATE TABLE match_team (
+CREATE TABLE match_participant (
+    match_participant_key   int             NOT NULL AUTO_INCREMENT,
     match_key               int             NOT NULL,
     team_key                int             NOT NULL,
-
-    PRIMARY KEY (match_key, team_key),
+    
+    PRIMARY KEY (match_participant_key),
     FOREIGN KEY (match_key) REFERENCES match(match_key),
     FOREIGN KEY (team_key) REFERENCES team(team_key)
 );
+
+-- Consider this table the members of any given team
+CREATE TABLE team_member (
+    team_member_key         int             NOT NULL AUTO_INCREMENT,
+    team_key                int             NOT NULL,
+    player_key              int             NOT NULL,
+    is_captain              tinyint         NULL,
+    
+    PRIMARY KEY (team_member_key),
+    FOREIGN KEY (team_key) REFERENCES team(team_key),
+    FOREIGN KEY (player_key) REFERENCES player(player_key)
+)
