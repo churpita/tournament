a3eac6b71170b973dcbd5ed04fa12b2a92b2ccdc
churpita <churpita@canisius.edu>
2023-08-11 18:34:40 -0400

    Delete model files (unused)

--- server/model/event.ts
+++
@@ -1,6 +0,0 @@
-export interface Event {
-    // Unique identifier for any given event
-    event_key: number;
-    // The name of the event
-    name: string;
-};
--- server/model/game.ts
+++
@@ -1,6 +0,0 @@
-export interface Game {
-    // Unique identifier for any given game
-    game_key: number;
-    // The name of the game
-    name: string;
-};
