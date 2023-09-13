237027bc6a5dfab1e7b84212f4a95bb645b277cd
Anthony Churpita <32397472+churpita@users.noreply.github.com>
2023-09-13 19:15:28 -0400

    Add tooltip support to IconButton

--- client/src/components/IconButton.tsx
+++ client/src/components/IconButton.tsx
@@ -6,12 +6,26 @@ type Props = {
     children: React.ReactNode;
     style?: React.CSSProperties;
     onClick?: React.MouseEventHandler;
+    tooltip?: {
+        text: string;
+        offset?: {
+            top?: string;
+            bottom?: string;
+            left?: string;
+            right?: string;
+        };
+    };
 };
 
 export const IconButton = (props: Props): React.ReactNode => {
     return (
         <div className={styles.icon} {...props}>
             {props.children}
+            {props.tooltip && (
+                <div className={`${styles.tooltipParent}`} style={props.tooltip.offset}>
+                    <span className={styles.tooltipText}>{props.tooltip.text}</span>
+                </div>
+            )}
         </div>
     );
 };
