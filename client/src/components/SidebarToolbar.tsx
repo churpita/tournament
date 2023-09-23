c99f386410699d5ec2bb793636ad3dd4f0f6514f
Anthony Churpita <32397472+churpita@users.noreply.github.com>
2023-09-23 12:26:27 -0400

    Add collapse all button (pending functionality)

--- client/src/components/SidebarToolbar.tsx
+++ client/src/components/SidebarToolbar.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 import styles from "./SidebarToolbar.module.css";
-import { MdAddCircle } from "react-icons/md";
+import { MdAddCircle, MdRemoveCircleOutline } from "react-icons/md";
 import IconButton from "./IconButton";
 
 export const SidebarToolbar = (): React.ReactNode => {
     return (
         <div className={styles.toolbar}>
             <IconButton
-                style={{ height: "1rem" }}
+                style={{ height: "1rem", paddingRight: "0.5rem" }}
                 tooltip={{
                     text: "Expand All",
                     offset: { bottom: "3rem" },
                 }}
             >
                 <MdAddCircle />
+            </IconButton>
+            <IconButton
+                style={{ height: "1rem" }}
+                tooltip={{
+                    text: "Collapse All",
+                    offset: { bottom: "3rem" },
+                }}
+            >
+                <MdRemoveCircleOutline />
             </IconButton>
         </div>
     );
